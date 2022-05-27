51f4db1c7d07ca0e4e60a92dd1adbdb5277fce0a
Jack <jack.hogg@bbc.co.uk>
2022-05-27 15:00:39 +0100

    icons

--- src/components.js
+++ src/components.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./index.css";
+import { TiArrowLeft, TiArrowRight } from "react-icons/ti";
+import { MdRestartAlt } from "react-icons/md";
 
 export function Square(props) {
   return (
@@ -44,8 +46,39 @@ export class Board extends React.Component {
 
 export function BackButton(props) {
 	return (
-		<button className="backButton" onClick={props.onClick}>
-			Back
-		</button>
+			<TiArrowLeft size={50} onClick={props.onClick}/>
 	);
 }
+
+export function ForwardButton(props) {
+	const color = props.clickable ? "" : "grey";
+	return (
+		<TiArrowRight size={50} color={color} onClick={props.onClick}/>
+	);
+}
+
+export function RestartButton(props) {
+	return (
+		<MdRestartAlt size={35} onClick={props.onClick}/>
+	);
+}
+
+export function Moves(props) {
+	const moves = props.history.map((_, move) => {
+      return (
+        <li key={move}>
+          <button onClick={() => {props.onClick(move)}}>#{move}</button>
+        </li>
+      );
+  });
+	return (
+		<div className="moves">
+		<h5>Moves</h5>
+		<ol>
+			{moves}
+		</ol>
+		</div>
+	);
+}
+			
+
--- src/game.js
+++ src/game.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./index.css";
-import { Board, BackButton } from "./components.js";
+import {
+  Board,
+  BackButton,
+  ForwardButton,
+  RestartButton,
+	Moves,
+} from "./components.js";
 import { calculateWinner } from "./utils.js";
-import { IoMdArrowBack } from "react-icons";
 
 export class Game extends React.Component {
   constructor(props) {
@@ -39,54 +44,63 @@ export class Game extends React.Component {
     });
   }
 
-	goBack() {
-		if (this.state.stepNumber > 0) {
-			this.jumpTo(this.state.stepNumber - 1)
-		}
-	}
+  goBack() {
+    if (this.state.stepNumber > 0) {
+      this.jumpTo(this.state.stepNumber - 1);
+    }
+  }
+
+  goFoward() {
+    if (this.state.stepNumber < this.state.history.length - 1) {
+      this.jumpTo(this.state.stepNumber + 1);
+    }
+  }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: step % 2 === 0,
-			winner: null,
+      winner: null,
     });
   }
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-
-    const moves = history.map((_, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
+   
     let status;
     if (this.state.winner) {
       status = "Winner is " + this.state.winner;
     } else {
-      status = "Player: " + (this.state.xIsNext ? "X" : "O");
+      status = (this.state.xIsNext ? "X" : "O") + " to move";
     }
+
+    const canGoForward =
+      this.state.stepNumber + 1 === history.length ? false : true;
 
     return (
       <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => {
-              this.handleClick(i);
-            }}
-          />
+        <div className="game-box">
+          <h3>{status}</h3>
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={(i) => {
+                this.handleClick(i);
+              }}
+            />
+            <div className="game-bar">
+              <BackButton onClick={() => this.goBack()} />
+              <ForwardButton
+                clickable={canGoForward}
+                onClick={() => this.goFoward()}
+              />
+              <RestartButton onClick={() => this.jumpTo(0)} />
+            </div>
+          </div>
         </div>
         <div className="game-info">
-          <div>{status}</div>
-					<BackButton onClick={() => this.goBack()}/>
-          <ol>{moves}</ol>
+						<Moves history={this.state.history} onClick={(step) => this.jumpTo(step)}/>
         </div>
       </div>
     );
